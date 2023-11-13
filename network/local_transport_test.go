46806fb566b16bb7e410b775b65779f8a04a5568
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-14 00:40:42 +0530

    typecasted Transport to *LocalTransport

--- network/local_transport_test.go
+++ network/local_transport_test.go
@@ -1,34 +1,33 @@
 package network
 
-//
-// import (
-// 	"testing"
-//
-// 	"github.com/stretchr/testify/assert"
-// )
-//
-// func TestConnect(t *testing.T) {
-// 	tra := NewLocalTransport("A")
-// 	trb := NewLocalTransport("B")
-//
-// 	tra.Connect(trb)
-// 	trb.Connect(tra)
-//
-// 	assert.Equal(t, tra.peers[trb.Addr()], trb)
-// 	assert.Equal(t, trb.peers[tra.Addr()], tra)
-// }
-//
-// func TestSendMessage(t *testing.T) {
-// 	tra := NewLocalTransport("A")
-// 	trb := NewLocalTransport("B")
-//
-// 	tra.Connect(trb)
-// 	trb.Connect(tra)
-//
-// 	msg := []byte("hello world")
-// 	assert.Nil(t, tra.SendMessage(trb.Addr(), msg))
-//
-// 	rpc := <-trb.Consume()
-// 	assert.Equal(t, rpc.Payload, msg)
-// 	assert.Equal(t, string(rpc.From), string(tra.Addr()))
-// }
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConnect(t *testing.T) {
+	tra := NewLocalTransport("A")
+	trb := NewLocalTransport("B")
+
+	tra.Connect(trb)
+	trb.Connect(tra)
+
+	assert.Equal(t, tra.(*LocalTransport).peers[trb.Addr()], trb)
+	assert.Equal(t, trb.(*LocalTransport).peers[tra.Addr()], tra)
+}
+
+func TestSendMessage(t *testing.T) {
+	tra := NewLocalTransport("A")
+	trb := NewLocalTransport("B")
+
+	tra.Connect(trb)
+	trb.Connect(tra)
+
+	msg := []byte("hello world")
+	assert.Nil(t, tra.SendMessage(trb.Addr(), msg))
+
+	rpc := <-trb.Consume()
+	assert.Equal(t, rpc.Payload, msg)
+	assert.Equal(t, string(rpc.From), string(tra.Addr()))
+}
