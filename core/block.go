dd001fb32395e5d37fee761ccb7b210f75f4ccf6
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-13 15:30:43 +0530

    block and transaction structs added

---
+++ core/block.go
@@ -0,0 +1,61 @@
+package core
+
+import (
+	"encoding/binary"
+	"io"
+
+	"github.com/wesley-lewis/go-blockchain/types"
+)
+
+type Header struct {
+	Version   uint32
+	PrevBlock types.Hash
+	Timestamp uint64
+	Height    uint32
+	Nonce     uint64
+}
+
+func (h *Header) EncodeBinary(w io.Writer) error {
+	if err := binary.Write(w, binary.LittleEndian, &h.Version); err != nil {
+		return err
+	}
+	if err := binary.Write(w, binary.LittleEndian, &h.PrevBlock); err != nil {
+		return err
+	}
+	if err := binary.Write(w, binary.LittleEndian, &h.Timestamp); err != nil {
+		return err
+	}
+	if err := binary.Write(w, binary.LittleEndian, &h.Height); err != nil {
+		return err
+	}
+	if err := binary.Write(w, binary.LittleEndian, &h.Nonce); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (h *Header) DecodeBinary(r io.Reader) error {
+	if err := binary.Read(r, binary.LittleEndian, &h.Version); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &h.PrevBlock); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &h.Timestamp); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &h.Height); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &h.Nonce); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type Block struct {
+	Header
+	Transaction []Transaction
+}
