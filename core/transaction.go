60be4c57c15108653a8eefa96c0bc752d44f7f04
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-14 20:08:04 +0530

    sign() transaction added

--- core/transaction.go
+++ core/transaction.go
@@ -20,3 +20,16 @@ func (tx *Transaction) DecodeBinary(r io.Reader) error {
 func (tx *Transaction) EncodeBinary(w io.Writer) error {
 	return nil
 }
+
+func (tx *Transaction) Sign(privKey crypto.PrivateKey) error {
+	sig, err := privKey.Sign(tx.Data)
+
+	if err != nil {
+		return err
+	}
+
+	tx.PublicKey = privKey.PublicKey()
+	tx.Signature = sig
+
+	return nil
+}
