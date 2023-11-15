286cd73d70edcccbeb780300b35a793cb8993271
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-15 22:49:31 +0530

    tested GetHeader func of Blockchain{}

--- core/blockchain_test.go
+++ core/blockchain_test.go
@@ -1,14 +1,13 @@
 package core
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func newBlockchainWithGenesis(t *testing.T) *Blockchain {
-	bc, err := NewBlockchain(randomBlock(0))
+	bc, err := NewBlockchain(randomBlock(1))
 	assert.Nil(t, err)
 
 	return bc
@@ -17,14 +16,14 @@ func newBlockchainWithGenesis(t *testing.T) *Blockchain {
 func TestAddBlock(t *testing.T) {
 	bc := newBlockchainWithGenesis(t)
 
-	totalBlocks := 1000
-	for i := 0; i < 1000; i++ {
-		block := randomBlockWithSignature(t, uint32(i+1))
+	totalBlocks := 10
+	for i := 2; i <= totalBlocks; i++ {
+		block := randomBlockWithSignature(t, uint32(i))
 		assert.Nil(t, bc.AddBlock(block))
 	}
 
 	assert.Equal(t, bc.Height(), uint32(totalBlocks))
-	assert.Equal(t, len(bc.headers), totalBlocks+1)
+	assert.Equal(t, len(bc.headers), totalBlocks)
 
 	assert.NotNil(t, bc.AddBlock(randomBlock(20)))
 }
@@ -32,12 +31,29 @@ func TestAddBlock(t *testing.T) {
 func TestNewBlockchain(t *testing.T) {
 	bc := newBlockchainWithGenesis(t)
 	assert.NotNil(t, bc.validator)
-	assert.Equal(t, bc.Height(), uint32(0))
-
-	fmt.Println(bc.Height())
+	assert.Equal(t, bc.Height(), uint32(1))
 }
 
 func TestHasBlock(t *testing.T) {
 	bc := newBlockchainWithGenesis(t)
 	assert.True(t, bc.HasBlock(0))
+}
+
+func TestGetHeader(t *testing.T) {
+	bc := newBlockchainWithGenesis(t)
+	lenBlocks := 1000
+
+	for i := 2; i < lenBlocks; i++ {
+		block := randomBlockWithSignature(t, uint32(i))
+		assert.Nil(t, bc.AddBlock(block))
+		header, err := bc.GetHeader(uint32(i))
+		assert.Nil(t, err)
+		assert.Equal(t, header, block.Header)
+	}
+}
+
+func TestAddBlockTooHigh(t *testing.T) {
+	bc := newBlockchainWithGenesis(t)
+
+	assert.NotNil(t, bc.AddBlock(randomBlockWithSignature(t, 3)))
 }
