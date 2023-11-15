7d8c615e5bccaac254d22c1d55487de985e68b7f
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-15 11:58:44 +0530

    Tested AddBlocks()

--- core/blockchain_test.go
+++ core/blockchain_test.go
@@ -7,12 +7,37 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBlockchain(t *testing.T) {
+func newBlockchainWithGenesis(t *testing.T) *Blockchain {
 	bc, err := NewBlockchain(randomBlock(0))
-	fmt.Println(bc)
 	assert.Nil(t, err)
+
+	return bc
+}
+
+func TestAddBlock(t *testing.T) {
+	bc := newBlockchainWithGenesis(t)
+
+	totalBlocks := 1000
+	for i := 0; i < 1000; i++ {
+		block := randomBlock(uint32(i + 1))
+		assert.Nil(t, bc.AddBlock(block))
+	}
+
+	assert.Equal(t, bc.Height(), uint32(totalBlocks))
+	assert.Equal(t, len(bc.headers), totalBlocks+1)
+
+	assert.NotNil(t, bc.AddBlock(randomBlock(20)))
+}
+
+func TestNewBlockchain(t *testing.T) {
+	bc := newBlockchainWithGenesis(t)
 	assert.NotNil(t, bc.validator)
 	assert.Equal(t, bc.Height(), uint32(0))
 
 	fmt.Println(bc.Height())
+}
+
+func TestHasBlock(t *testing.T) {
+	bc := newBlockchainWithGenesis(t)
+	assert.True(t, bc.HasBlock(0))
 }
