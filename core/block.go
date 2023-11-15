8b5fc3a210a9714da24f31ac1bdb11f454b33122
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-15 23:58:39 +0530

    prevBlockHash included while creation of blocks

--- core/block.go
+++ core/block.go
@@ -11,11 +11,19 @@ import (
 )
 
 type Header struct {
-	Version   uint32
-	DataHash  types.Hash
-	PrevBlock types.Hash
-	Timestamp int64
-	Height    uint32
+	Version       uint32
+	DataHash      types.Hash
+	PrevBlockHash types.Hash
+	Timestamp     int64
+	Height        uint32
+}
+
+func (h *Header) Bytes() []byte {
+	buf := &bytes.Buffer{}
+	enc := gob.NewEncoder(buf)
+	enc.Encode(h)
+
+	return buf.Bytes()
 }
 
 type Block struct {
@@ -36,7 +44,7 @@ func NewBlock(h *Header, txx []Transaction) *Block {
 }
 
 func (b *Block) Sign(privKey crypto.PrivateKey) error {
-	sig, err := privKey.Sign(b.HeaderData())
+	sig, err := privKey.Sign(b.Header.Bytes())
 	if err != nil {
 		return err
 	}
@@ -52,7 +60,7 @@ func (b *Block) Verify() error {
 		return fmt.Errorf("Block has no signature")
 	}
 
-	if !b.Signature.Verify(b.Validator, b.HeaderData()) {
+	if !b.Signature.Verify(b.Validator, b.Header.Bytes()) {
 		return fmt.Errorf("block has invalid signature")
 	}
 
@@ -68,19 +76,10 @@ func (b *Block) Encode(w io.Writer, enc Encoder[*Block]) error {
 	return nil
 }
 
-func (b *Block) Hash(hasher Hasher[*Block]) types.Hash {
+func (b *Block) Hash(hasher Hasher[*Header]) types.Hash {
 	if b.hash.IsZero() {
-		b.hash = hasher.Hash(b)
+		b.hash = hasher.Hash(b.Header)
 	}
 
 	return b.hash
-}
-
-func (b *Block) HeaderData() []byte {
-	buf := &bytes.Buffer{}
-
-	enc := gob.NewEncoder(buf)
-	enc.Encode(b.Header)
-
-	return buf.Bytes()
 }
