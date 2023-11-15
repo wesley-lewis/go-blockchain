2fa25e1ba8d2a08cedcf56ca2f20329ac7d30843
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-15 11:59:38 +0530

    completed the validator()

--- core/validator.go
+++ core/validator.go
@@ -1,5 +1,7 @@
 package core
 
+import "fmt"
+
 type Validator interface {
 	ValidateBlock(*Block) error
 }
@@ -15,5 +17,8 @@ func NewBlockValidator(bc *Blockchain) *BlockValidator {
 }
 
 func (v *BlockValidator) ValidateBlock(b *Block) error {
+	if v.bc.HasBlock(b.Height) {
+		return fmt.Errorf("chain already contains block {%d} with hash {%s}", b.Height, b.Hash(BlockHasher{}))
+	}
 	return nil
 }
