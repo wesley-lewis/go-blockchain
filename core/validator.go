673d54278c865b318472bf066b1fe15177551d8d
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-15 11:23:05 +0530

    Default Validator added

---
+++ core/validator.go
@@ -0,0 +1,19 @@
+package core
+
+type Validator interface {
+	ValidateBlock(*Block) error
+}
+
+type BlockValidator struct {
+	bc *Blockchain
+}
+
+func NewBlockValidator(bc *Blockchain) *BlockValidator {
+	return &BlockValidator{
+		bc: bc,
+	}
+}
+
+func (v *BlockValidator) ValidateBlock(b *Block) error {
+	return nil
+}
