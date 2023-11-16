0526b1cd0b3eaba778945e04923e59a2167d1ef9
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-16 12:39:02 +0530

    tests for mempool

---
+++ network/txpool_test.go
@@ -0,0 +1,26 @@
+package network
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/wesley-lewis/go-blockchain/core"
+)
+
+func TestTxPool(t *testing.T) {
+	p := NewTxPool()
+
+	assert.Equal(t, p.Len(), 0)
+
+}
+
+func TestTxPoolAddTx(t *testing.T) {
+	p := NewTxPool()
+	tx := core.NewTransaction([]byte("foo"))
+
+	assert.Nil(t, p.Add(tx))
+	assert.Equal(t, p.Len(), 1)
+
+	p.Flush()
+	assert.Equal(t, p.Len(), 0)
+}
